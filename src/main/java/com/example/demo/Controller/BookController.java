fa726ae50b2854e5e3f2924f40ec78611b465cbd
Abhishek Abhimanyu <abhishekabhimanyu26@gmail.com>
2019-12-15 18:19:01 +0530

    Spring CRUD Update

--- src/main/java/com/example/demo/Controller/BookController.java
+++ src/main/java/com/example/demo/Controller/BookController.java
@@ -1,9 +1,11 @@
 package com.example.demo.Controller;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +27,13 @@ public class BookController {
 	public List<Books> getBookList(){
 		return bookRepo.findAll();
 		}
+	
+	//Display only one books by id
+		@GetMapping("/{id}")
+		public Optional<Books> getBook(@PathVariable("id") Long id)
+		{
+			return bookRepo.findById(id);
+			}
 
 	//Add book details
 		@RequestMapping(value= "/", method=RequestMethod.POST,consumes={"application/json"})
@@ -33,5 +42,20 @@ public class BookController {
 			return bookRepo.save(newbook);
 			
 		}
-		}	
+		
+		//Edit book details
+				@RequestMapping(value= "/{id}", method=RequestMethod.PATCH,consumes={"application/json"})
+				public Books editBook(@RequestBody Books newbook)
+				{
+					return bookRepo.save(newbook);
+				}
+		
+				//Delete book details
+				@RequestMapping(value= "/{id}", method=RequestMethod.DELETE)
+				public String deleteBook(@RequestBody Books newbook)
+				{
+					 bookRepo.delete(newbook);
+					 return "Delete Successful";
+				}
+}	
 	
